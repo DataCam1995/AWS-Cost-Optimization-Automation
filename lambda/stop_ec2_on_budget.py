a6a67ab0c58aaff4e1515563218ac9a247dabc46
DataCam1995 <106635580+DataCam1995@users.noreply.github.com>
2025-06-19 17:14:45 -0400

    Create stop_ec2_on_budget.py

---
+++ lambda/stop_ec2_on_budget.py
@@ -0,0 +1,9 @@
+import boto3
+
+ec2 = boto3.client('ec2')
+instances = ['i-0123456789abcdef0']  # Replace with real instance IDs
+
+def lambda_handler(event, context):
+    response = ec2.stop_instances(InstanceIds=instances)
+    print(f"Stopped EC2 Instances: {instances}")
+    return response
