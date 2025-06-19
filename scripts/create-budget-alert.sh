259a70fa988d71fce5f28159573caef07e37f948
DataCam1995 <106635580+DataCam1995@users.noreply.github.com>
2025-06-19 17:15:47 -0400

    Update create-budget-alert.sh

--- scripts/create-budget-alert.sh
+++ scripts/create-budget-alert.sh
@@ -4,3 +4,4 @@ aws budgets create-budget \
   --account-id YOUR_ACCOUNT_ID \
   --budget file://budget.json \
   --notifications-with-subscribers file://notification.json
+Add script to create AWS budget and alert policy
